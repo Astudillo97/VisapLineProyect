b544c46e3d37942f8f5eb0eb37d113bf920b5e31
Yeferson Astudillo <y.astudillo@udla.edu.co>
2018-02-19 16:36:13 -0500

    Actualizacion 19022018

--- VisapLine/Model/Permisos.cs
+++ VisapLine/Model/Permisos.cs
@@ -13,9 +13,9 @@ namespace VisapLine.Model
         IData data = new Data();
         public string idpermisos { get; set; }
         public string rol_idrol { get; set; }
-        public string permisos{ get; set; }
+        public string permisos { get; set; }
         public string menu_idmenu { get; set; }
-
+        Menu men = new Menu();
         public DataTable ConsultarPermisos()
         {
             return data.ConsultarDatos("");
@@ -23,11 +23,33 @@ namespace VisapLine.Model
 
         public bool RegistrarPermisos(Permisos per)
         {
-            return data.OperarDatos("select * from public.pr_insertarpermiso("+per.rol_idrol+",'"+per.permisos+"',"+per.menu_idmenu+")");
+            return data.OperarDatos("select * from public.pr_insertarpermiso(" + per.rol_idrol + ",'" + per.permisos + "'," + per.menu_idmenu + ")");
         }
-        public DataTable ConsultarRolPermisos(Permisos per)
+        public DataTable ConsultarRolPermisos(string rol)
         {
-            return data.ConsultarDatos("select * from pr_consularmenu(" + per.rol_idrol + ")");
+            return data.ConsultarDatos("select * from pr_consularmenu(" + rol + ")");
+        }
+
+        public bool ValidarPermisos(string url, DataTable entrada)
+        {
+            bool ret = false;
+            DataTable dat = ConsultarRolPermisos(entrada.Rows[0]["idrol"].ToString());
+
+
+            for (int i = 0; i < dat.Rows.Count && ret == false; i++)
+            {
+                men.menu_idmenusub = dat.Rows[i]["idmenu"].ToString();
+                DataTable sub = men.ConsultarMenuSubmenu(men);
+                for (int j = 0; j < sub.Rows.Count && ret == false; j++)
+                {
+                    if (dat.Rows[i]["href"].ToString().Equals(url))
+                    {
+                        ret = true;
+                        break;
+                    }
+                }
+            }
+            return ret;
         }
     }
 }
---
+++ VisapLine/View/Private/Error.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VisaPlane.visapline.Private
+{
+    public partial class Error1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string data = Convert.ToString(Request.QueryString["error"]);
+            if (data != null)
+            {
+                error_s.Text = data;
+            }
+            Session.Abandon();
+        }
+    }
+}
