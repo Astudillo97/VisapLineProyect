e25c04fd4b60545d0e41d9954c011a08282534f0
Yeferson Astudillo <y.astudillo@udla.edu.co>
2018-02-27 18:35:18 -0500

    Actualizacion27022018
    Actualizacion

--- VisapLine/View/Private/pagplan.aspx.cs
+++ VisapLine/View/Private/pagplan.aspx.cs
@@ -42,7 +42,6 @@ namespace VisapLine.View.Private
                         }
                         catch (Exception)
                         {
-
                             throw;
                         }
              
