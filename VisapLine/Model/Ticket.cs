b5705eff51a9a5179c82a1b1ac02c0f7b5b66c13
Marx sebastian Barahona torres <baradona69@gmail.com>
2018-04-21 17:40:51 -0500

    creacion modelo ticket

---
+++ VisapLine/Model/Ticket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VisapLine.DataAccess.Data;
+using VisapLine.DataAccess.Connection;
+using System.Data;
+
+namespace VisapLine.Model
+{
+    public class Ticket
+    {
+        IData data = new Data();
+        
+        public string idTicket { get; set; }
+        public string fechaCreacionTicket { get; set; }
+        public string empaTicket { get; set; }
+        public string estaTicket { get; set; }
+        public string TipologiaFK { get; set; }
+        public string ServiciosFK { get; set; }
+        public string MedioAtencionFK { get; set; }
+        public string EmisorTicketFK { get; set; }
+        public string RespuestaTicketFK { get; set; }
+        public string AreasFK { get; set; }
+        public string DescTicket { get; set; }
+        public string tipoTicket { get; set; }
+
+        public DataTable RegistrarTicket()
+        {
+            return data.OperarDatos("select * from public.Insertar_Ticket('"++"')");
+        }
+
+        
+    }
+}
